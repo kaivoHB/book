2cf3cd91f0083c5816020a39e27cc3ff8fa1cf4f
Kai Vo <bao.typingcode@gmail.com>
2023-10-27 10:51:58 +0700

    fixed deploy to detail.tsx

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -31,7 +31,7 @@ export default function Detail(props:any) {
     const [data, setData] = useState<Data | null>(null);
 
     useEffect(() => {
-        axios.get(`http://vuquanghuydev.pythonanywhere.com/api/book/${id}/`)
+        axios.get(`https://vuquanghuydev.pythonanywhere.com/api/book/${id}/`)
             .then((response) => {
                 setData(response.data);            
             })
